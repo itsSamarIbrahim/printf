dbfd43e82ce0302dbc0d4991bcb64cc2a2da65e7
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-15 12:56:34 -0700

    Creating a file for our putchar function

---
+++ putchar_.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * putchar_ - an implementation for the real putchar function
+ * @c: write this character for the stdout
+ * Return: nothing - void
+ */
+void putchar_(char c)
+{
+	int index = 0;
+
+	while (index < 1)
+	{
+		write(1, &c, 1);
+		index++;
+	}
+}
