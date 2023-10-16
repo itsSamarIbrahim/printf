031e01d0d9c54cd63a8d7ce0e0173d1d501b33ca
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-15 17:36:32 -0700

    Adding a new function to the file putchar_.c

--- putchar_.c
+++ putchar_.c
@@ -13,7 +13,24 @@ void putchar_(char c)
 
 	while (index < 1)
 	{
-		write (1, &c, 1);
+		write(1, &c, 1);
 		index++;
 	}
+}
+
+/**
+ * putchars - an implementation for the real puthcar, help with(%d, i%)
+ * @c: write this character for the stdout
+ * Return: nothing - void
+ */
+void putchars(char c)
+{
+	unsigned char unsigned_ch;
+
+	if (write(1, &c, 1) != 1)
+		return (EOF);
+
+	unsigned_ch = c;
+	return (unsigned_ch);
+
 }
