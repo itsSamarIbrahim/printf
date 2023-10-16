836bdf1c174031fa7b67849d4f17cce58e0c7608
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-16 13:31:29 -0700

    Edit 0-printf_c_s_prcnt.c file

--- 0-printf_c_s_prcnt.c
+++ 0-printf_c_s_prcnt.c
@@ -8,7 +8,7 @@
  */
 int _printf(const char *format, ...)
 {
-	int index = 0, store, j;
+	int index = 0, store, j, increment = 0;
 	char *ptrStr;
 	va_list printf;
 
@@ -21,22 +21,34 @@ int _printf(const char *format, ...)
 			{
 				store = va_arg(printf, int);
 				putchar_(store);
+				increment++;
 				index++;
 			}
 			else if ('s' == format[index + 1] && '%' == format[index])
 			{
 				ptrStr = va_arg(printf, char *);
 				for (j = 0; ptrStr[j] != 0; j++)
+				{
 					putchar_(ptrStr[j]);
+					increment++;
+				}
 				index++;
 			}
 			else
-				putchar_(format[index]);
+			{
+				putchar_('%');
+				putchar_(format[index + 1]);
+				increment += 2;
+				index++;
+			}
 		}
 		else
+		{
 			putchar_(format[index]);
+			increment++;
+		}
 	}
 	va_end(printf);
 
-	return (0);
+	return (increment);
 }
