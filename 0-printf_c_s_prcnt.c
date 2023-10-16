54f81c33fdf1c4addeae83b7c6cf3d3c15f1c55e
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-16 13:09:58 -0700

    Edit 0-printf_c_s_prcnt.c

--- 0-printf_c_s_prcnt.c
+++ 0-printf_c_s_prcnt.c
@@ -15,18 +15,23 @@ int _printf(const char *format, ...)
 	va_start(printf, format);
 	for (index = 0; format[index] != 0; index++)
 	{
-		if ('c' == format[index + 1] && '%' == format[index])
+		if (format[index + 1] != 0 && '%' == format[index])
 		{
-			store = va_arg(printf, int);
-			putchar_(store);
-			index++;
-		}
-		else if ('s' == format[index + 1] && '%' == format[index])
-		{
-			ptrStr = va_arg(printf, char *);
-			for (j = 0; ptrStr[j] != 0; j++)
-				putchar_(ptrStr[j]);
-			index++;
+			if ('c' == format[index + 1])
+			{
+				store = va_arg(printf, int);
+				putchar_(store);
+				index++;
+			}
+			else if ('s' == format[index + 1] && '%' == format[index])
+			{
+				ptrStr = va_arg(printf, char *);
+				for (j = 0; ptrStr[j] != 0; j++)
+					putchar_(ptrStr[j]);
+				index++;
+			}
+			else
+				putchar_(format[index]);
 		}
 		else
 			putchar_(format[index]);
