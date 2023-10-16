e55b79c651d41d170670fcb07a59aeee8883b18a
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-16 13:37:59 -0700

    Edit 0-printf_c_s_prcnt.c file

--- 0-printf_c_s_prcnt.c
+++ 0-printf_c_s_prcnt.c
@@ -27,8 +27,11 @@ int _printf(const char *format, ...)
 			else if ('s' == format[index + 1] && '%' == format[index])
 			{
 				ptrStr = va_arg(printf, char *);
-				for (j = 0; ptrStr[j] != 0; j++; increment++)
+				for (j = 0; ptrStr[j] != 0; j++)
+				{
 					putchar_(ptrStr[j]);
+					increment++;
+				}
 				index++;
 			}
 			else
