eecbf947d83727b40996c5190f62a7fb155f3b66
Samar <bantmasr21@gmail.com>
2023-10-17 16:07:54 -0700

    -

--- 1-print_int.c
+++ 1-print_int.c
@@ -8,6 +8,7 @@
 int print_int(va_list args)
 {
 	int value, divisor = 1, digit, digitCount = 0;
+	unsigned int number;
 
 	value = va_arg(args, int);
 
@@ -18,15 +19,18 @@ int print_int(va_list args)
 	}
 	if (value < 0)
 	{
-		value = -value;
-		_putchar('-');
-		digitCount = digitCount + _putchar(48);
+		/*value = -value;
+		_putchar('-');*/
+		digitCount = digitCount + _putchar(45);
+		number = (-1) * value;
 	}
+	else
+		number = value;
 	while (value / divisor > 9)
 	{
 		divisor = divisor * 10;
 	}
-	while (divisor != 0)
+	while (divisor > 0)
 	{
 		digit = value / divisor;
 		_putchar(48 + digit);
