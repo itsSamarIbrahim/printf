11ed002f33805c31c74ddbd233990d17ffae641e
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-17 06:57:43 -0700

    Add some files

--- main.h
+++ main.h
@@ -9,5 +9,8 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 int print_int(va_list args);
 int printf_c_s_prcnt(const char *format, ...);
+int print_char(va_list args);
+int print_str(va_list args);
+int print_percent(__attribute__((unused))va_list args);
 
 #endif
