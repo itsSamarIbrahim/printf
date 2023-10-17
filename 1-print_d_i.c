b8b683093607f47cc985c01cf4555dca5d25b1a2
Samar <bantmasr21@gmail.com>
2023-10-17 06:41:25 -0700

    adding 1-print_d_i.c

--- 1-print_d_i.c
+++ 1-print_d_i.c
@@ -1,67 +1,32 @@
 #include "main.h"
-#include <stdio.h>
-#include <unistd.h>
 
 /**
- * printf_d_i - a function helps to print numbers with %d and %i specifiers
- * @format: a pointer to the string
- * Return: the number of characters printed
+ * print_int - a function to print integers
+ * @args: arguments' list
+ * Return: divisor
  */
-int printf_d_i(const char *format, ...)
+int print_int(va_list args)
 {
-	va_list args;
-	int a, char_printed = 0, prod, divisor = 1, digit;
+	int value, divisor = 1, digit;
 
-	va_start(args, format);
-	for (a = 0; format[a] != '\0' && format != 0; a++)
+	value = va_arg(args, int);
+
+	if (value < 0)
 	{
-		if (format[a] == '%')
-		{
-			switch (format[a + 1])
-			{
-
-				case 'i':
-				case 'd':
-					{
-						prod = va_arg(args, int);
-						if (prod < 0)
-						{
-							putchar_('-');
-							prod = -prod;
-							char_printed++;
-						}
-						while (prod / divisor > 9)
-						{
-							divisor = divisor * 10;
-						}
-						while (divisor != 0)
-						{
-							digit = prod / divisor;
-							putchar_('0' + digit);
-							prod = prod % divisor;
-							divisor = divisor / 10;
-							char_printed++;
-						}
-						a++;
-						break;
-					}
-				default:
-					{
-						putchar_('%');
-						putchar_(format[a + 1]);
-						char_printed = char_printed + 2;
-						a++;
-						break;
-					}
-			}
-		}
-		else
-		{
-			putchar_(format[a]);
-			char_printed++;
-		}
+		value = -value;
+		_putchar('-');
 	}
-	va_end(args);
+	while (value / divisor >= 10)
+	{
+		divisor = divisor * 10;
+	}
+	while (divisor > 0)
+	{
+		digit = value / divisor;
+		_putchar(48 + digit);
+		value = value % divisor;
+		divisor = divisor / 10;
+	}
 
-	return (char_printed);
+	return (divisor);
 }
