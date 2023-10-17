88d162ac071863713495da660ed2832a79607c95
Samar <bantmasr21@gmail.com>
2023-10-17 05:43:48 -0700

    -

---
+++ _putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _putchar - a function prints a character
+ * @c: The character to be printed
+ * Return: the character
+ */
+int _putchar(char c)
+{
+	return putc(c, stdout);
+}
