3f9dac4458fdfca595d01b103d4c68641ffbaf83
Samar <bantmasr21@gmail.com>
2023-10-17 09:50:52 -0700

    updated

--- 0-print_percent.c
+++ 0-print_percent.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- *
+ * print_percent - a function that helps with printing a percent sign
+ * @args: arguments' list
+ * Return: 1 on success
  */
 int print_percent(__attribute__((unused))va_list args)
 {
