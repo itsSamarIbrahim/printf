a0ec3dc92df060da6e1e6e190e5bc6a6c5b9256a
Samar <bantmasr21@gmail.com>
2023-10-17 10:54:55 -0700

    updated

--- 1-print_int.c
+++ 1-print_int.c
@@ -22,7 +22,7 @@ int print_int(va_list args)
 		_putchar('-');
 		digitCount = digitCount + _putchar(48);
 	}
-	while (divisor <= value / 10)
+	while (value / divisor >= 10)
 	{
 		divisor = divisor * 10;
 	}
