a750a0ce53414f2ea10c9e56d4a634172692a4e0
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-16 18:30:04 -0700

    Edit 0-printf_c_s_prcnt.c file

--- 0-printf_c_s_prcnt.c
+++ 0-printf_c_s_prcnt.c
@@ -1,7 +1,5 @@
 #include "main.h"
 #include <stdarg.h>
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
  * _printf - implementing the %s and %c from the real printf function
@@ -10,61 +8,46 @@
  */
 int _printf(const char *format, ...)
 {
-	int index = 0, store, j, increment = 0;
+	int index = 0, store, increment = 0;
 	char *ptrStr;
 	va_list printf;
 
 	va_start(printf, format);
-	for (index = 0; format[index] != 0; index++)
+	for (index = 0; format[index] != 0; ++index)
 	{
 		if (format[index + 1] != 0 && '%' == format[index])
 		{
-			if (format[index + 1] != 0 && '%' == format[index])
+			if ('c' == format[index + 1])
 			{
-				if ('c' == format[index + 1])
+				putchar_(va_arg(printf, int));
+				increment++;
+				index++;
+			}
+			else if ('s' == format[index + 1])
+			{
+				for (*ptrStr = va_arg(args, char *); *ptrStr != '\0'; ptrStr++)
 				{
-					store = va_arg(printf, int);
-					if (putchar_(store) == EOF)
-						return (EOF);
+					putchar_(*ptrStr);
 					increment++;
-					index++;
 				}
-				else if ('s' == format[index + 1])
-				{
-					ptrStr = va_arg(printf, char *);
-					for (j = 0; ptrStr[j] != 0; j++)
-					{
-						if (putchar_(ptrStr[j]) == EOF)
-							return (EOF);
-						increment++;
-					}
-					index++;
-				}
-				else if ('%' == format[index + 1])
-				{
-					if (putchar_('%') == EOF)
-						return (EOF);
-					putchar_(format[index + 1]);
-					increment += 2;
-					index++;
-				}
-				else
-				{
-					if (putchar_('%') == EOF || putchar_(format[index + 1]) == EOF)
-						return EOF;
-					increment += 2;
-					index++;
-				}
+				index++;
 			}
 			else
 			{
-				if (putchar_(format[index]) == EOF)
-					return EOF;
-				increment++;
+				putchar_('%');
+				putchar_(format[index + 1]);
+				increment += 2;
+				index++;
 			}
+			++i;
 		}
-		va_end(printf);
-
-		return (increment);
+		else
+		{
+			putchar_(format[index]);
+			increment++;
+		}
 	}
+	va_end(printf);
+
+	return (increment);
 }
