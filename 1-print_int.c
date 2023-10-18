67724952847ce03a91cd5e348442dc81c69c56bd
Samar <bantmasr21@gmail.com>
2023-10-17 19:17:48 -0700

    -

--- 1-print_int.c
+++ 1-print_int.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * print_int - a function helps to print an integer
+ * print_int - a function to print integers
  * @args: arguments' list
- * Return: the count of the digits
+ * Return: divisor
  */
 int print_int(va_list args)
 {
-	int value, divisor = 1, digitCount = 0;
-	unsigned int number;
+	int value, divisor = 1, digit, digitCount = 0;
 
 	value = va_arg(args, int);
+
 	if (value == 0)
 	{
 		_putchar('0');
@@ -18,18 +18,20 @@ int print_int(va_list args)
 	}
 	if (value < 0)
 	{
-		digitCount += _putchar('-');
-		number = value * -1;
+		/*_putchar('-');*/
+		digitCount = digitCount + _putchar('-');
+		/*number = (-1) * value;*/
+		value = -value;
 	}
-	else
-		number = value;
-	while (number / divisor > 9)
-		divisor *= 10;
+	while (value / divisor > 9)
+		divisor = divisor * 10;
 	while (divisor > 0)
 	{
-		digitCount += _putchar('0' + number / divisor);
-		number %= divisor;
-		divisor /= 10;
+		digit = value / divisor;
+		_putchar('0' + digit);
+		value = value % divisor;
+		divisor = divisor / 10;
+		digitCount++;
 	}
 
 	return (digitCount);
